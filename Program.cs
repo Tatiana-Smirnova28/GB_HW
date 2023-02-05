d015562c6369b7fee98a7daf20774359720f47e1
Tatiana <tanya.tany2001@mail.ru>
2023-02-05 17:41:49 +0300

    task1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Task 1
+Console.Clear();
+
+Console.Write("First number");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Second number");
+int m = Convert.ToInt32(Console.ReadLine());
+
+
+if (n>m)
+    Console.WriteLine($"max = {n}");
+
+else if (n<m)
+    Console.WriteLine($"max = {m}");
+
+else
+    Console.WriteLine("equal");
